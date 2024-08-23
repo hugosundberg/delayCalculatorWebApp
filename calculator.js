d0cd0bdb8e2c4a4637afe52a94e836c62e575b80
hugosundberg <hugosundberg98@gmail.com>
2024-08-23 20:13:28 +0200

    add second values

--- calculator.js
+++ calculator.js
@@ -1,9 +1,14 @@
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('bpm').focus(); // Automatically focus the input field on page load
+});
+
 document.addEventListener('keydown', function(event) {
     if (event.key === 'Enter') {
         calculateNoteValues(); 
     }
 
 }); 
+
 
 function calculateNoteValues() {
     const bpm = document.getElementById("bpm").value;
@@ -15,14 +20,20 @@ function calculateNoteValues() {
     const halfNote = (quarterNote * 2).toFixed(0);
     const wholeNote = (quarterNote * 4).toFixed(0);
     const eighthNote = (quarterNote / 2).toFixed(0);
-    const sixteenthNote = (quarterNote / 4).toFixed(0)
-    ;
+    const sixteenthNote = (quarterNote / 4).toFixed(0);
 
-    document.getElementById("wholeNote").innerText = wholeNote;
-    document.getElementById("halfNote").innerText = halfNote;
-    document.getElementById("quarterNote").innerText = quarterNote;
-    document.getElementById("eighthNote").innerText = eighthNote;
-    document.getElementById("sixteenthNote").innerText = sixteenthNote;
+    document.getElementById("wholeNoteMs").innerText = wholeNote;
+    document.getElementById("halfNoteMs").innerText = halfNote;
+    document.getElementById("quarterNoteMs").innerText = quarterNote;
+    document.getElementById("eighthNoteMs").innerText = eighthNote;
+    document.getElementById("sixteenthNoteMs").innerText = sixteenthNote;
+
+    document.getElementById("wholeNoteSec").innerText = wholeNote / 1000; 
+    document.getElementById("halfNoteSec").innerText = halfNote / 1000; 
+    document.getElementById("quarterNoteSec").innerText = quarterNote / 1000; 
+    document.getElementById("eighthNoteSec").innerText = eighthNote / 1000; 
+    document.getElementById("sixteenthNoteSec").innerText = sixteenthNote / 1000; 
+    
 
     document.getElementById("results").style.display = "table";
 }
