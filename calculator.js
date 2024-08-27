a60255d54a55d20eaba7dad4397f6526bc88a3a6
hugosundberg <hugosundberg98@gmail.com>
2024-08-27 20:24:17 +0200

    straight and dotted fixed

--- calculator.js
+++ calculator.js
@@ -1,30 +1,33 @@
 document.addEventListener('DOMContentLoaded', function() {
     document.getElementById('bpm').focus(); // Automatically focus the input field on page load
-    
-    // Get all radio buttons with the name 'noteType'
-    const noteTypeRadioButtons = document.querySelectorAll('input[name="noteType"]');
-
-    noteTypeRadioButtons.forEach(radio => {
-        radio.addEventListener('change', function(event) {
-            // When a different radio button is selected
-            const selectedValue = event.target.value;
-        });
-    });
 });
 
 document.addEventListener('keydown', function(event) {
     if (event.key === 'Enter') {
-        calculateNoteValues(); 
+        const selectedValue = document.querySelector('input[name="noteType"]:checked').value;
+        calculateNoteValues(selectedValue); 
     }
 }); 
 
-function calculateNoteValues() {
+// Get all radio buttons with the name 'noteType'
+const noteTypeRadioButtons = document.querySelectorAll('input[name="noteType"]');
+
+noteTypeRadioButtons.forEach(radio => {
+    radio.addEventListener('change', function(event) {
+        // When a different radio button is selected
+        const selectedValue = event.target.value;
+        calculateNoteValues(selectedValue);
+    });
+});
+
+function calculateNoteValues(selectedValue) {
     const bpm = document.getElementById("bpm").value;
-
-
+    
     if (bpm <= 0 || isNaN(bpm)) {
         return;
     }
+
+    console.log(selectedValue);
 
     // Calculates millisecond values
     const quarterNote = (60000 / bpm).toFixed(0); // milliseconds for a quarter note
@@ -32,38 +35,44 @@ function calculateNoteValues() {
     const wholeNote = (quarterNote * 4).toFixed(0);
     const eighthNote = (quarterNote / 2).toFixed(0);
     const sixteenthNote = (quarterNote / 4).toFixed(0);
+    const thirtySecondNote = (quarterNote / 8).toFixed(0);
+
 
     if (selectedValue === "straight"){
-        // Displays millisecond values
+        // Displays straight millisecond values
         document.getElementById("wholeNoteMs").innerText = wholeNote;
         document.getElementById("halfNoteMs").innerText = halfNote;
         document.getElementById("quarterNoteMs").innerText = quarterNote;
         document.getElementById("eighthNoteMs").innerText = eighthNote;
         document.getElementById("sixteenthNoteMs").innerText = sixteenthNote;
+        document.getElementById("thirtySecondNoteMs").innerText = thirtySecondNote;
 
-        // Displays second values
+        // Displays straight second values
         document.getElementById("wholeNoteSec").innerText = wholeNote / 1000; 
         document.getElementById("halfNoteSec").innerText = halfNote / 1000; 
         document.getElementById("quarterNoteSec").innerText = quarterNote / 1000; 
         document.getElementById("eighthNoteSec").innerText = eighthNote / 1000; 
         document.getElementById("sixteenthNoteSec").innerText = sixteenthNote / 1000; 
+        document.getElementById("thirtySecondNoteSec").innerText = thirtySecondNote / 1000; 
+
         
-    } else if (selectedValue == "dotted") {
-         // Displays millisecond values
-         document.getElementById("wholeNoteMs").innerText = wholeNote * 1.5;
+    } else if (selectedValue === "dotted") {
+         // Displays dotted millisecond values
+         document.getElementById("wholeNoteMs").innerText = (wholeNote * 1.5);
          document.getElementById("halfNoteMs").innerText = halfNote * 1.5;
          document.getElementById("quarterNoteMs").innerText = quarterNote * 1.5;
          document.getElementById("eighthNoteMs").innerText = eighthNote * 1.5;
          document.getElementById("sixteenthNoteMs").innerText = sixteenthNote * 1.5;
-
-         console.log("selected value = dotted")
  
-         // Displays second values
+         // Displays dotted second values
          document.getElementById("wholeNoteSec").innerText = (wholeNote / 1000) * 1.5; 
          document.getElementById("halfNoteSec").innerText = (halfNote / 1000) * 1.5; 
          document.getElementById("quarterNoteSec").innerText = (quarterNote / 1000) * 1.5; 
          document.getElementById("eighthNoteSec").innerText = (eighthNote / 1000) * 1.5; 
          document.getElementById("sixteenthNoteSec").innerText = (sixteenthNote / 1000) *1.5; 
+    } else {
+        // Displays triplet millisecond values
+
     }
 
     
